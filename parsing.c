82de2f613a1bccb9adf8574005e08e620f4e6e25
nawfallodhi <nawfal.lodhi@gmail.com>
2026-05-04 22:11:08 +0500

    Changing Lval Struct Definition and Constructors/Destructors

--- parsing.c
+++ parsing.c
@@ -6,26 +6,69 @@ static char input[2048];
 
 enum{LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM};
 
-enum{LVAL_NUM, LVAL_ERR};
+enum{LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR};
 
-typedef struct{
+typedef struct lval{
     int type;
     long num;
-    int err;
+    char* err;
+    char* sym;
+    int count;
+    struct lval** cell;
 } lval;
 
-lval lval_num(long x){
-    lval v;
-    v.type = LVAL_NUM;
-    v.num = x;
+lval* lval_num(long x){
+    lval* v = malloc(sizeof(lval));
+    v->type = LVAL_NUM;
+    v->num = x;
     return v;
 }
 
-lval lval_err(int x){
-    lval v;
-    v.type = LVAL_ERR;
-    v.err = x;
+lval* lval_err(char* m){
+    lval* v= malloc(sizeof(lval));
+    v->type = LVAL_ERR;
+    v->err = malloc(strlen(m)+1);
+    strcpy(v->err,m);
     return v;
+}
+
+lval* lval_sym(char* s) {
+  lval* v = malloc(sizeof(lval));
+  v->type = LVAL_SYM;
+  v->sym = malloc(strlen(s) + 1);
+  strcpy(v->sym, s);
+  return v;
+}
+
+lval* lval_sexpr(void) {
+  lval* v = malloc(sizeof(lval));
+  v->type = LVAL_SEXPR;
+  v->count = 0;
+  v->cell = NULL;
+  return v;
+}
+
+void lval_del(lval* v){
+    switch(v->type){
+        case LVAL_NUM: 
+            break;
+
+        case LVAL_ERR:
+            free(v->err);
+            break;
+
+        case LVAL_SYM:
+            free(v->sym);
+            break;
+        
+        case LVAL_SEXPR:
+            for (int i=0;i<v->count;i++){
+                lval_del(v->cell[i]);
+            }
+            free(v->cell);
+            break;
+    }
+    free(v);
 }
 
 void lval_print(lval v){
@@ -91,7 +134,8 @@ int main(int argc, char** argv){
 
     /*Create some parsers*/
     mpc_parser_t* Number = mpc_new("number");
-    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Symbol = mpc_new("Symbol");
+    mpc_parser_t* Sexpr = mpc_new("sexpr");
     mpc_parser_t* Expr = mpc_new("expr");
     mpc_parser_t* Lispy = mpc_new("lispy");
 
@@ -99,11 +143,12 @@ int main(int argc, char** argv){
     mpca_lang(MPCA_LANG_DEFAULT,
     "                                                       \
         number   : /-?[0-9]+/ ;                             \
-        operator : '+' | '-' | '*' | '/' ;                  \
-        expr     : <number> | '(' <operator> <expr>+ ')' ;  \
-        lispy    : /^/ <operator> <expr>+ /$/ ;             \
+        symbol   : '+' | '-' | '*' | '/' ;                  \
+        sexpr    : '(' <expr>* ')' ;                        \
+        expr     : <number> | <symbol> | <sexpr>;           \
+        lispy    : /^/ <expr>* /$/ ;                        \
     ",
-    Number, Operator, Expr, Lispy);
+    Number, Symbol, Sexpr, Expr, Lispy);
     
 
     puts("Lispy version 0.0.0.0.1");
@@ -129,7 +174,7 @@ int main(int argc, char** argv){
     }
 
     //Cleaning up pointers
-    mpc_cleanup(4, Number, Operator, Expr, Lispy);
+    mpc_cleanup(4, Number, Symbol, Sexpr, Expr, Lispy);
 
     return 0;
 }
