2d1e03ad06ce68241ecd852b7bedf8f356a94fca
nawfallodhi <nawfal.lodhi@gmail.com>
2026-05-05 13:04:40 +0500

    Added functions to evulate Sexpr and operations

--- parsing.c
+++ parsing.c
@@ -85,6 +85,28 @@ lval* lval_add(lval* v, lval* x){
     return v;
 }
 
+lval* lval_pop(lval* v, int i) {
+    /* Find the item at "i" */
+    lval* x = v->cell[i];
+
+    /* Shift memory after the item at "i" over the top */
+    memmove(&v->cell[i], &v->cell[i+1],
+        sizeof(lval*) * (v->count-i-1));
+
+    /* Decrease the count of items in the list */
+    v->count--;
+
+    /* Reallocate the memory used */
+    v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+    return x;
+}
+
+lval* lval_take(lval* v, int i) {
+    lval* x = lval_pop(v, i);
+    lval_del(v);
+    return x;
+}
+
 lval* lval_read(mpc_ast_t* t){
     
     if(strstr(t->tag,"number")){
@@ -141,46 +163,91 @@ void lval_print(lval* v) {
   }
 }
 
+lval* builtin_op(lval* a, char* op) {
+
+    /* Ensure all arguments are numbers */
+    for (int i = 0; i < a->count; i++) {
+        if (a->cell[i]->type != LVAL_NUM) {
+        lval_del(a);
+        return lval_err("Cannot operate on non-number!");
+        }
+    }
+
+    /* Pop the first element */
+    lval* x = lval_pop(a, 0);
+
+    /* If no arguments and sub then perform unary negation */
+    if ((strcmp(op, "-") == 0) && a->count == 0) {
+        x->num = -x->num;
+    }
+
+    /* While there are still elements remaining */
+    while (a->count > 0) {
+
+        /* Pop the next element */
+        lval* y = lval_pop(a, 0);
+
+        if (strcmp(op, "+") == 0) { x->num += y->num; }
+        if (strcmp(op, "-") == 0) { x->num -= y->num; }
+        if (strcmp(op, "*") == 0) { x->num *= y->num; }
+        if (strcmp(op, "/") == 0) {
+        if (y->num == 0) {
+            lval_del(x); lval_del(y);
+            x = lval_err("Division By Zero!"); break;
+        }
+        x->num /= y->num;
+        }
+
+        lval_del(y);
+    }
+
+    lval_del(a); return x;
+}
 
 void lval_println(lval* v){
     lval_print(v);
     putchar('\n');
 }
 
-// lval eval_op(lval x, char* op, lval y){
-//     if(x.type == LVAL_ERR){return x;}
-//     if(y.type == LVAL_ERR){return y;}
-        
-//     if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
-//     if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
-//     if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
-//     if (strcmp(op, "/") == 0) {
-//         /* If second operand is zero return error */
-//         return y.num == 0
-//         ? lval_err(LERR_DIV_ZERO)
-//         : lval_num(x.num / y.num);
-//     }
+lval* lval_eval(lval* v);
 
-//     return lval_err(LERR_BAD_OP);
-// }
+lval* lval_eval_sexpr(lval* v){
+    for (int i=0;i<v->count;i++){
+        v->cell[i] = lval_eval(v->cell[i]);
+    }
 
-// lval eval(mpc_ast_t* t){
-//     if (strstr(t->tag, "number")){
-//         errno=0;
-//         long x = strtol(t->contents,NULL,10);
-//         return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
-//     }
+    for (int i = 0; i < v->count; i++) {
+        if (v->cell[i]->type == LVAL_ERR) { 
+            return lval_take(v, i); 
+        }
+    }
 
-//     char* op = t->children[1]->contents;
-//     lval x = eval(t->children[2]);
+    if (v->count == 0){ 
+        return v; 
+    }
 
-//     int i=3;
-//     while(strstr(t->children[i]->tag,"expr")){
-//         x=eval_op(x,op,eval(t->children[i]));
-//         i++;
-//     }
-//     return x;
-// }
+    if(v->count == 1){ 
+        return lval_take(v, 0); 
+    }
+
+    lval* f = lval_pop(v, 0);
+    if (f->type != LVAL_SYM) {
+        lval_del(f); lval_del(v);
+        return lval_err("S-expression Does not start with symbol!");
+    }
+
+    /* Call builtin with operator */
+    lval* result = builtin_op(v, f->sym);
+    lval_del(f);
+    return result;
+}
+
+lval* lval_eval(lval* v) {
+    if (v->type == LVAL_SEXPR){ 
+        return lval_eval_sexpr(v); 
+    }
+    return v;
+}
 
 int main(int argc, char** argv){
 
@@ -212,12 +279,10 @@ int main(int argc, char** argv){
 
         fgets(input, 2048, stdin);
 
-        //printf("No you're a %s", input);
-
         mpc_result_t r;
 
         if (mpc_parse("<stdin>",input,Lispy, &r)){
-            lval* x = lval_read(r.output);
+            lval* x = lval_eval(lval_read(r.output));
             lval_println(x);
             lval_del(x);
             mpc_ast_delete(r.output);
